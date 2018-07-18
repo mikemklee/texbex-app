481dbed0495fbc7ca8fe3617ae715e1369ed8b93
Mike <munkyu0813@gmail.com>
2018-07-17 20:49:22 -0400

    cleanup boilerplate code from create-react-app and add sass support to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import "./styles/css/index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept();
+}
