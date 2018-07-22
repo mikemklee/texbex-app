3188b04cc9cd871a457d08bbb1cc08fee18fa8cd
Mike <munkyu0813@gmail.com>
2018-07-22 14:13:32 -0400

    add and style ModalContact component

--- client/src/components/books/BooksItem.jsx
+++ client/src/components/books/BooksItem.jsx
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import moment from "moment";
 import numeral from "numeral";
 import _ from "lodash";
 
-export default ({ book }) => {
-  return (
-    <div className="books__item">
-      <div className="books__item__title">{_.startCase(book.title)}</div>
-      <div className="books__item__price">
-        {numeral(book.price / 100).format("$0,0.00")}
+// components
+import ModalContact from "components/modals/ModalContact";
+
+class BooksItem extends Component {
+  state = {
+    showModal: false
+  };
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { book } = this.props;
+    return (
+      <div className="books__item">
+        <div className="books__item__title">{_.startCase(book.title)}</div>
+        <div className="books__item__price">
+          {numeral(book.price / 100).format("$0,0.00")}
+        </div>
+        <div className="books__item__course">
+          <span>{book.course.toUpperCase()}</span>
+          {book.school && <span>({book.school})</span>}
+        </div>
+        <div className="books__item__desc">{book.description}</div>
+        <div className="books__item__date">
+          {moment(book.date).format("Do MMMM, YYYY")}
+        </div>
+        <div className="books__item__actions">
+          <button className="books__item__actions--keep">
+            <i className="far fa-heart" />
+            <span>Keep</span>
+          </button>
+          <button
+            className="books__item__actions--contact"
+            onClick={this.handleOpenModal}
+          >
+            Contact
+          </button>
+          <ModalContact
+            isOpen={this.state.showModal}
+            handleClose={this.handleCloseModal}
+            book={book}
+          />
+        </div>
       </div>
-      <div className="books__item__course">
-        <span>{book.course.toUpperCase()}</span>
-        {book.school && <span>({book.school})</span>}
-      </div>
-      <div className="books__item__desc">{book.description}</div>
-      <div className="books__item__date">
-        {moment(book.date).format("Do MMMM, YYYY")}
-      </div>
-      <div className="books__item__actions">
-        <button className="books__item__actions--keep">
-          <i className="far fa-heart" />
-          <span>Keep</span>
-        </button>
-        <button className="books__item__actions--contact">Contact</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default BooksItem;
---
+++ client/src/components/modals/ModalContact.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import Modal from "react-modal";
+import moment from "moment";
+import numeral from "numeral";
+import _ from "lodash";
+
+// components
+import TextAreaField from "../common/TextAreaField";
+
+// Modal Configuration
+Modal.setAppElement("#root");
+
+class ModalContact extends Component {
+  state = {
+    message: "",
+    date: moment(),
+    errors: {}
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const { user } = this.props.auth;
+    const newMessage = {
+      message: this.state.message,
+      date: this.state.date,
+      buyer: user.name,
+      buyerPhoto: user.avatar
+    };
+    console.log(newMessage);
+  };
+  render() {
+    const { isOpen, handleClose, book } = this.props;
+    const { errors } = this.state;
+
+    return (
+      <Modal
+        className="modalContact"
+        isOpen={isOpen}
+        onRequestClose={handleClose}
+        shouldCloseOnOverlayClick={true}
+        shouldCloseOnEsc={true}
+      >
+        <div className="modalContact__title">{_.startCase(book.title)}</div>
+        <button className="modalContact__close" onClick={handleClose}>
+          &times;
+        </button>
+        <div className="modalContact__course">
+          <span>{book.course.toUpperCase()}</span>
+          {book.school && <span>({book.school})</span>}
+        </div>
+        <div className="modalContact__desc">{book.description}</div>
+        <div className="modalContact__date">
+          {moment(book.date).format("Do MMMM, YYYY")}
+        </div>
+        <div className="modalContact__price">
+          {numeral(book.price / 100).format("$0,0.00")}
+        </div>
+        <div className="modalContact__sellerInfo">
+          <div className="modalContact__sellerInfo--heading">
+            <p>Contact seller directly or send an inquiry:</p>
+          </div>
+          <div className="modalContact__sellerInfo--email">
+            Email: test@test.com
+          </div>
+          <div className="modalContact__sellerInfo--phone">
+            Phone: 6476421813
+          </div>
+        </div>
+        <div className="modalContact__inquiry">
+          <TextAreaField
+            placeholder="Write a short message to the seller"
+            name="message"
+            value={this.state.message}
+            onChange={this.onChange}
+            error={errors.message}
+          />
+          <div className="modalContact__inquiry--submit">Send</div>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default ModalContact;
